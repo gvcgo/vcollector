547f3e4d62e60b1aae227da3b567c390e8be9135
moqsien <moqsien@foxmail.com>
2024-05-22 12:06:18 +0800

    update

--- pkgs/crawlers/mix/erlang.go
+++ pkgs/crawlers/mix/erlang.go
@@ -1,1 +1,15 @@
 package mix
+
+import (
+	"github.com/gvcgo/vcollector/pkgs/version"
+)
+
+/*
+For windows:
+https://github.com/erlang/otp/releases
+*/
+type Erlang struct {
+	SDKName  string
+	RepoName string
+	Version  version.VersionList
+}
--- pkgs/crawlers/mix/php.go
+++ pkgs/crawlers/mix/php.go
@@ -1,1 +1,13 @@
 package mix
+
+import "github.com/gvcgo/vcollector/pkgs/version"
+
+/*
+for windows:
+https://windows.php.net/downloads/releases/archives/
+*/
+type PHP struct {
+	SDKName     string
+	DownloadUrl string
+	Version     version.VersionList
+}
