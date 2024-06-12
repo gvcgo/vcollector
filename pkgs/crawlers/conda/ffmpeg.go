26c861c3c2bbcf125eab4b67aced24841a218964
moqsien <moqsien2022@gmail.com>
2024-06-12 10:12:41 +0800

    ffmpeg

---
+++ pkgs/crawlers/conda/ffmpeg.go
@@ -0,0 +1,53 @@
+package conda
+
+import (
+	"github.com/gvcgo/vcollector/internal/iconf"
+	"github.com/gvcgo/vcollector/pkgs/crawlers/crawler"
+	"github.com/gvcgo/vcollector/pkgs/version"
+)
+
+func init() {
+	crawler.RegisterCrawler(NewFFMPeg())
+}
+
+type FFMPeg struct {
+	SDKName string
+	CondaSearcher
+}
+
+func NewFFMPeg() (f *FFMPeg) {
+	f = &FFMPeg{
+		SDKName: "ffmpeg",
+		CondaSearcher: CondaSearcher{
+			Version: make(version.VersionList),
+		},
+	}
+	return
+}
+
+func (f *FFMPeg) GetSDKName() string {
+	return f.SDKName
+}
+
+func (f *FFMPeg) Start() {
+	f.CondaSearcher.Search(f.SDKName)
+}
+
+func (f *FFMPeg) GetVersions() []byte {
+	r, _ := f.Version.Marshal()
+	return r
+}
+
+func (f *FFMPeg) HomePage() string {
+	return "https://ffmpeg.org/"
+}
+
+func (f *FFMPeg) GetInstallConf() (ic iconf.InstallerConfig) {
+	return iconf.InstallerConfig{
+		BinaryDirs: &iconf.DirItems{
+			Windows: []iconf.DirPath{{"Library", "bin"}},
+			MacOS:   []iconf.DirPath{{"bin"}},
+			Linux:   []iconf.DirPath{{"bin"}},
+		},
+	}
+}
