a24b9e2efd03b66945d8094069db0cf2a18950b8
moqsien <moqsien@foxmail.com>
2024-07-02 11:14:04 +0800

    image proxy

---
+++ docs/vmr_images.js
@@ -0,0 +1,48 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run "npm run dev" in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run "npm run deploy" to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+
+async function getResponse(dUrl) {
+    try {
+        let response = await fetch(dUrl);
+        return response;
+    } catch (error) {
+        console.log('get allowed_sites failed', error);
+        let resp  = makeRes("get installation script failed!", 502)
+        return ;
+    }
+}
+  
+function makeRes(body, status = 200, headers = {}) {
+    headers['access-control-allow-origin'] = '*'
+    return new Response(body, {status, headers})
+}
+
+// https://github.com/gvcgo/vmrdocs/blob/main/src/assets/vmr.gif
+
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+})
+    
+async function handleRequest(request) {
+    let rUrl = new URL(request.url)
+
+    fileName = rUrl.pathname.split("/").pop()
+ 
+    let response = makeRes("download failed", 502)
+    const downloadUrl = "https://github.com/gvcgo/vmrdocs/blob/main/src/assets/"
+    if (fileName) {
+        response = await getResponse(downloadUrl + fileName)
+    }
+    
+    const modifiedResponse = new Response(response.body, response);
+    modifiedResponse.headers.set('Access-Control-Allow-Origin', '*');
+    return modifiedResponse;
+}
